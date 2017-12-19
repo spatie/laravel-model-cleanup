8bf02fa6f0fcb5397ecf69e2ffeb87c6cd388743
Francis Lavoie <lavofr@gmail.com>
2017-12-19 07:01:12 -0500

    Write tests to assert my changes

---
+++ tests/FindModelsFromConfigTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Spatie\ModelCleanup\Test;
+
+use Spatie\ModelCleanup\ModelWasCleanedUp;
+use Spatie\ModelCleanup\CleanUpModelsCommand;
+use Spatie\ModelCleanup\Test\Models\CleanableItem;
+use Spatie\ModelCleanup\Test\Models\UncleanableItem;
+use Illuminate\Contracts\Console\Kernel;
+
+class FindModelsFromConfigTest extends TestCase
+{
+    /** @test */
+    public function it_can_find_class_name_from_file()
+    {
+        $method = self::getMethod(CleanUpModelsCommand::class, 'getFullyQualifiedClassNameFromFile');
+        $cmd = new CleanUpModelsCommand(app()->make('files'));
+
+        $className = $method->invokeArgs($cmd, ["./tests/Models/CleanableItem.php"]);
+
+        $this->assertTrue($className !== "");
+        
+        $className = $method->invokeArgs($cmd, ["./tests/Models/NotAClass.php"]);
+
+        $this->assertTrue($className === "");
+    }
+
+    /** @test */
+    public function it_can_find_class_names_from_directory()
+    {
+        $method = self::getMethod(CleanUpModelsCommand::class, 'getClassNamesInDirectory');
+        $cmd = new CleanUpModelsCommand(app()->make('files'));
+
+        $classNames = $method->invokeArgs($cmd, ["./tests/Models"]);
+
+        $this->assertContains(CleanableItem::class, $classNames);
+
+        $this->assertNotContains(null, $classNames);
+    }
+
+    protected static function getMethod($class, $name)
+    {
+        $class = new \ReflectionClass($class);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
+}
